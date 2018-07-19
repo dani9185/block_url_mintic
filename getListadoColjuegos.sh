b143993f1e6cc97b650cf119eb13a24e77eaa90a
juancra264 <juancra264@hotmail.com>
2018-07-18 19:00:59 -0500

    Se adiciona comentarios

--- getListadoColjuegos.sh
+++ getListadoColjuegos.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
+# ------------------------------------------------------------------------------
+# Fecha inicial: 18 Julio 2018
+# Descarga automatica de listado URL a bloquear Coljuegos de MINTIC
+#
+# Instruccion para la ejecucion (Permitir la ejecucion del script)
+#   chmod +x <script>.sh
+#   ./<script>.sh 
+# ------------------------------------------------------------------------------
 
 
+# Variales con informacion de Acceso MIN TIC
 URL_MINTIC="http://archivo.mintic.gov.co/ubpi/664/articles-75802_archivo_txt.txt"
 User="ArturoCuellar"
 Pass="ArturoCuellar6542"
 
+# Revisa si la url puede descargar el listado o problemas en el usuario y pass
 OUTPUT="$(wget -SO- -T 1 -t 1 --spider --user=${User} --password=${Pass} ${URL_MINTIC} 2>&1 >/dev/null | egrep -i "404|Authentication Fail")"
-
 if [ -z "${OUTPUT}" ]; then
-	wget -q --user=${User} --password=${Pass} --output-document=listadoColjuegos_new.txt ${URL_MINTIC}
+    wget -q --user=${User} --password=${Pass} --output-document=listadoColjuegos_new.txt ${URL_MINTIC}
 	md5sum listadoColjuegos.txt | awk {'print $1}' > checksum_listadoColjuegos
 	md5sum listadoColjuegos_new.txt | awk {'print $1}' > checksum_listadoColjuegos_new
 	OUTPUT="$(diff -q checksum_listadoColjuegos checksum_listadoColjuegos_new)"
